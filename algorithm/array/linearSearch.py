abc1f1a2e95d3d26847a5bc3c2152768996083c3
Gautam Hirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-07-13 22:24:36 +0530

    linearSearch.py

---
+++ algorithm/array/linearSearch.py
@@ -0,0 +1,35 @@
+"""
+Algo :- 
+
+define l_search(arr,value){
+    for 0 <= i <= length(arr):
+        if arr[i] == value 
+        return true
+    return false
+}
+"""
+
+# Defining function
+
+def linearSearch(arr,val):
+ for i in range(len(arr)):
+    if arr[i] == val:
+        return True
+ return False
+
+
+
+#------------------------------------
+# TESTING 
+
+arr1 = [1,2,5,8,6,8,43,2,7,1,9,1]
+print(linearSearch(arr1,5))
+
+arr2 = [1,2,5,8,6,8,43,2,7,1,9,1]
+print(linearSearch(arr2,55))
+
+arr3 = [1,2,5,8,6,8,43,2,7,1,9,1]
+print(linearSearch(arr3,7))
+
+arr4 = [1,2,5,8,6,8,43,2,7,1,9,1]
+print(linearSearch(arr4,9))
