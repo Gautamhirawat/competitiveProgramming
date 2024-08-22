3534a0badebb44c4805fe04768fb7cccf819e63d
Gautam Hirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-08-22 21:29:38 +0530

    Update arrayImplemented.py

--- algorithm/Queue/arrayImplemented.py
+++ algorithm/Queue/arrayImplemented.py
@@ -1,16 +1,13 @@
 class Queue:
     def __init__(self):
         self.queue = list()
-
     
     def Print(self):
         return print(self.queue)
         
-
     def enqueue(self,element):
         self.queue.insert(0,element)
-        
-    
+          
     def dequeue(self):
         return print(self.queue.pop())
         
